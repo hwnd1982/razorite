b37faadc4dd15a7f47cf0b29739329e7b9a5ef14
Kirill Lavrov <lkv@myttk.ru>
2022-02-07 21:36:17 +0700

    feat (...): connecting styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "regenerator-runtime/runtime";
 import './index.html';
 import './fonts/NoirPro/stylesheet.css';
+import './css/normalize.css';
+import './css/style.css';
+import './sass/style.sass';
